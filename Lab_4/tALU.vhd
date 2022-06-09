55b5fd2ead750cc4c26796e3469c770362d34932
KhoiTrinh <97624649+KhoiTrinh@users.noreply.github.com>
2022-06-09 10:42:26 -0700

    Update tALU.vhd

--- Lab_4/tALU.vhd
+++ Lab_4/tALU.vhd
@@ -50,12 +50,54 @@ BEGIN
 		wait for 100 ns;
 
 		-- Start testing the ALU
-		datain_a <= X"01234567";	-- DataIn in hex
-		datain_b <= X"11223344";
-		control  <= "00000";		-- Control in binary (ADD and ADDI test)
-		wait for 20 ns; 			-- result = 0x124578AB  and zeroOut = 0
+		datain_a <= X"00000005";	-- DataIn in hex
+		datain_b <= X"00000002";
+		control  <= "00000";		-- Control in binary (ADD test)
+		wait for 100 ns; 			-- result = 0x00000007  and zeroOut = 0
 
-		-- Add test cases here to drive the ALU implementation
+		datain_a <= x"00000007";
+		datain_b <= x"00000001";
+		control <= "00001";		--SUB test
+		wait for 100 ns;			--result = 00000006 and zeroOut = 0
+
+		datain_a <= x"00000001";
+		datain_b <= x"00000001";
+		control <= "00010";		--ADDI test
+		wait for 100 ns;			--result = 0x00000002 and zeroOut = 0
+
+		datain_a <= "11111111111111111111111111111111" ;
+		datain_b <= "10101010101010101010101010101010" ;
+		control <= "00011";		--AND test
+		wait for 100 ns;			--result = 0xAAAAAAAA and zeroOut = 0
+
+		control <= "00100";		--ANDI test
+		wait for 100 ns;                        --result = 10101010101010101010101010101010 and zeroOut = 0
+
+		datain_a <= x"50505050";
+		datain_b <= x"05050505";
+		control <= "00101";		--OR test
+							--result = 0x55555555 and zeroOut = 0
+		wait for 100 ns;
+
+		control <= "00110";		--ORI test
+		wait for 100 ns;
+
+		datain_a <= x"00000001";
+		datain_b <= x"00000003";	--sll test
+		control <= "00111";			--result = 0x0x00000008
+		wait for 100 ns;
+
+		control <= "01000";		--slli test
+		wait for 100 ns;
+
+		datain_a <= X"00000008";
+		datain_b <= X"00000003";
+		control <= "00100";		--srl test
+		wait for 100 ns;			--result = 0x00000001  and zeroOut = 0
+
+		control <= "01010";		--srli test
+		wait for 100 ns;
+
 
 
 		wait; -- will wait forever
